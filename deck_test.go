c38d27b71d3b6f7a1a1f0646f505b15358d4bc51
Kyrylo Seliukov <kyrylo.seliukov@globallogic.com>
2024-09-20 16:52:54 +0200

    Add some tests

--- deck_test.go
+++ deck_test.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"os"
+	"testing"
+)
 
-func test() {
-	fmt.Println("florida")
+func TestNewDeck(t *testing.T) {
+	deck := newDeck()
+	expectedLen := 52
+
+	if len(deck) != expectedLen {
+		t.Errorf("expected deck length of %v but got %v", expectedLen, len(deck))
+	}
+	expectedFirstCard := "Ace of Spades"
+
+	if deck[0] != expectedFirstCard {
+		t.Errorf("Expect first card to be %v but got %v", expectedFirstCard, deck[0])
+	}
+	expectedLastCard := "Two of Clubs"
+
+	if deck[len(deck)-1] != expectedLastCard {
+		t.Errorf("Expect last card to be %v but got %v", expectedLastCard, deck[len(deck)-1])
+	}
+}
+
+func TestSaveToFileAndNewDeckFromFile(t *testing.T) {
+	filename := "_decktesting"
+	os.Remove(filename)
+
+	deck := newDeck()
+	deck.saveToFile(filename)
+
+	loadedDeck := newDeckFromFile(filename)
+	expectedLen := 52
+	if len(loadedDeck) != expectedLen {
+		t.Errorf("Expected %v cards in deck, got %v", expectedLen, len(loadedDeck))
+	}
+
+	os.Remove(filename)
 }
