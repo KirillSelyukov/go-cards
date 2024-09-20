1e3046597e9dee92ecebbf188b0359ef67118d45
Kyrylo Seliukov <kyrylo.seliukov@globallogic.com>
2024-09-20 17:58:19 +0200

    add person structure and receiver for it

--- main.go
+++ main.go
@@ -2,16 +2,17 @@ package main
 
 func main() {
 
-	cards1 := newDeck()
-	cards1.saveToFile("my_cards")
+	// cards1 := newDeck()
+	// cards1.saveToFile("my_cards")
 
-	cards := newDeckFromFile("my_cards")
-	cards.print()
-	cards.shuffle()
-	cards.print()
+	// cards := newDeckFromFile("my_cards")
+	// cards.print()
+	// cards.shuffle()
+	// cards.print()
 	// hand, rest := deal(cards, 3)
 	// hand.print()
 	// rest.print()
-	evenOdd()
+	// evenOdd()
+	buildPerson()
 
 }
